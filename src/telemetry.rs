e34b1d74a81c776b8b837d2a1f9ee7a7b6aed779
simrat12 <simratsaran@gmail.com>
2025-09-26 01:06:07 +0100

    feat: integrating grafanta/prometheus

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -17,12 +17,12 @@ pub fn init_metrics() {
     use metrics_exporter_prometheus::PrometheusBuilder;
 
     PrometheusBuilder::new()
-        .with_http_listener(([127, 0, 0, 1], 9000))
+        .with_http_listener(([0, 0, 0, 0], 9000))
         .install()
         .expect("prometheus exporter install");
 
     // prove the exporter is alive
-    println!("Prometheus exporter listening on http://127.0.0.1:9000/metrics");
+    println!("Prometheus exporter listening on http://0.0.0.0:9000/metrics");
     metrics::gauge!("lobx_up").set(1.0);
 }
 
